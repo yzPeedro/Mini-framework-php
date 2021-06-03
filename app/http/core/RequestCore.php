91e5f1a8f2064867e84819f602da8a6a70916838
yzPeedro <pedrocruzpessoa16@gmail.com>
2021-06-03 01:31:39 -0300

    add: comments to increase de code quality

--- app/http/core/RequestCore.php
+++ app/http/core/RequestCore.php
@@ -6,11 +6,24 @@ namespace app\http\core;
 
 class RequestCore
 {
+
+    /**
+     * Get GET params on actual REQUEST
+     * 
+     * @param $index
+     * @return string|array
+    */
     public function get($index = '')
     {
-        return (isset($_GET[$index])) ? $_GET[$index] : $_GET;
+        return (isset($_REQUEST["GET"][$index])) ? $_REQUEST["GET"][$index] : $_REQUEST["GET"];
     }
 
+    /**
+     * Get POST params on actual REQUEST
+     * 
+     * @param $index
+     * @return string|array
+    */
     public function post($index = '')
     {
         return (isset($_POST[$index])) ? $_POST[$index] : $_POST;
